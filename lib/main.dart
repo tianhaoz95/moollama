736b2c9e10e6464836d2aef0305abbbbb322a631
Tianhao Zhou <tianhaozhou95@icloud.com>
2025-08-31 00:24:38 -0700

    feat: show transcript

--- lib/main.dart
+++ lib/main.dart
@@ -197,6 +197,7 @@ class _SecretAgentHomeState extends State<SecretAgentHome> {
           setState(() {
             _lastWords = result.recognizedWords;
           });
+          _listeningPopupEntry?.markNeedsBuild();
         },
         listenFor: const Duration(seconds: 30),
         pauseFor: const Duration(seconds: 5),
